9f220331a4f7da94541b5f961ee7bdfd653deb55
Nishut Suman Mahunta <nishutsuman1998@gmail.com>
2022-03-03 02:55:24 +0530

    Day 01
    Day 01 assignment submitted.

---
+++ DAY 01/main.js
@@ -0,0 +1,12 @@
+let addition= require("./add")
+let substraction= require("./substract")
+let multiplication= require("./multiply")
+let divison= require("./divide")
+
+let num1=7;
+let num2= 5;
+
+console.log(addition(num1,num2))
+console.log(substraction(num1,num2))
+console.log(multiplication(num1,num2))
+console.log(divison(num1,num2))
---
+++ DAY 01/substract.js
@@ -0,0 +1,5 @@
+function substraction(x,y){
+    return x-y;
+}
+
+module.exports= substraction;
