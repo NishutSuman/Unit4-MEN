69c7680cc733aeb45bad0b0eadc620d592bacd90
Nishut Suman Mahunta <nishutsuman1998@gmail.com>
2022-03-07 12:31:17 +0530

    Unit 4 C-1 Code File
    Unit 4 C1 file submitted..I will upload this link in link submit section.

---
+++ Unit4 C1/index.js
@@ -0,0 +1,48 @@
+const express= require("express");
+const req = require("express/lib/request");
+const app= express();
+
+app.use(allLogger);
+// app.use(checkPermission)
+
+app.get("/books",(req,res)=>{
+    return res.send({route: "/books"});
+})
+app.get("/libraries",checkPermission('librarian'),(req,res)=>{
+    return res.send({ route: "/libraries", permission: req.allow});
+})
+app.get("/authors",checkPermission('author'),(req,res)=>{
+    return res.send({ route: "/authors", permission: req.allow});
+})
+
+function allLogger(req,res,next){
+    console.log(" all Logger is running...");
+    next();
+    console.log(" all Logger finished...");
+}
+
+function checkPermission(role){
+    if(role=='librarian'){
+        return function checkPermission(req,res,next){
+            if(req.path==="/libraries"){
+                req.allow=true;
+                console.log('Check Permission for library is running...')
+            }
+            next()
+            console.log("Library handler finished...")
+        }
+    }else if(role=='author'){
+        return function checkPermission(req,res,next){
+            if(req.path==="/authors"){
+                req.allow=true;
+                console.log('Check Permission for author is running...')
+            }
+            next()
+            console.log("Author handler finished...")
+        }
+    }
+}
+
+app.listen("5000",()=>{
+    console.log("Listening at port 5000...")
+})
